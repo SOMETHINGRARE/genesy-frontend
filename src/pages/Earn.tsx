78ac8921cd47252fea52fb5d5f565d422b5a285d
Huy Do <herohthd@gmail.com>
2023-11-29 18:00:59 +0700

    add margin for earn page

--- src/pages/Earn.tsx
+++ src/pages/Earn.tsx
@@ -22,7 +22,7 @@ const Earn = () => {
                 }}
               >
             <div
-                className="absolute left-[40px] top-[60px] w-[17px] h-[17px] border-white border-solid border cursor-pointer"
+                className="absolute left-[40px] top-[40px] w-[17px] h-[17px] border-white border-solid border cursor-pointer"
                 onMouseEnter={handleHover}
                 onMouseLeave={handleMouseLeave}
                 >
@@ -41,11 +41,11 @@ const Earn = () => {
         />
         </div>
         <div className="font-semibold text-[30px]">EARN KEYS</div>
-        <div className="text-[65px]">THE GAME HAS BEGUN</div>
-        <div>
+        <div className="mt-[60px] text-[65px]">THE GAME HAS BEGUN</div>
+        <div className="mt-[60px]">
             <img height="80" width="160" src={code} alt="KEYS token logo"></img>
         </div>
-        <div className="font-semibold text-[20px]">**EACH ARTWORK YOU COLLECT GET 10% OF THE VALUE IN KEYS</div>
+        <div className="mt-[80px] font-semibold text-[20px]">**EACH ARTWORK YOU COLLECT GET 10% OF THE VALUE IN KEYS</div>
       </div>
     );
   };
